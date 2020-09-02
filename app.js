0cd0ffe555fdcd552d123c69376c1b3ac114e087
Sergio <sergioa.orozcoo@gmail.com>
2020-09-01 20:03:23 -0500

    push

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>¡Hola Mundo!</h1>");
+  res.send("<h1>Hola Mundo</h1>");
 });
 app.listen(3000, () => console.log("Listening on port 3000!"));
